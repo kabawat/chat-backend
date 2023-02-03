ece2a317ed1ed156952cc5130969817262640e8e
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-02-04 00:11:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,24 +11,14 @@ const port = process.env.PORT || 2917
 const { socketModal } = require('./controller/connection')
 app.use(express.json())
 const corsOptions = {
-    origin:"https://queryboat.netlify.app",
-    credentials: true,
+    origin: "https://queryboat.netlify.app",
+    credentials: true
 };
 
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
-const io = new socketIO.Server(server,{
-cors: {
-        origin: "https://queryboat.netlify.app",
-        methods: ['GET', 'POST']
-    }
-})
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://queryboat.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+const io = socketIO(server)
+io.origins(["https://queryboat.netlify.app"])
 app.use(cookieParser())
 app.use(userAuth)
 
