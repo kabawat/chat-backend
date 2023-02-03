1a44697574edbe83024bb09cfb7699525f9aebee
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-02-03 23:58:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
+const io = new socketIO.Server(server)
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://queryboat.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "https://queryboat.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-const io = new socketIO.Server(server)
-
 app.use(cookieParser())
 app.use(userAuth)
 
