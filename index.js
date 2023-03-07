95e86d97078c82c5aa10178ad0f8369abaa670da
w3App <kshatriyakabawat@gmail.com>
2023-03-07 12:26:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors())
-app.post("/", (req, res) => {
+const http = require('http')
+const server = http.createServer(app)
+const socketIO = require('socket.io')
+
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(cors())
+
+server.get("/", (req, res) => {
     res.send("hello word")
 })
-app.listen(process.env.PORT, () => {
+
+server.post("/", (req, res) => {
+    res.send(req.body)
+})
+server.listen(process.env.PORT, () => {
     console.log(`click here http://localhost:${process.env.PORT}`)
 })
