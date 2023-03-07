7258970a5acee32edd30ac462971d25cbcd1cee7
w3App <kshatriyakabawat@gmail.com>
2023-03-07 12:49:41 +0530

    update package

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-const io = socketIO.Server(server, {
+const io = new socketIO.Server(server, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
