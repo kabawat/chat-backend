f2d822bc9f4a9b42f739cd7147fbce9c31e24dd5
w3App <kshatriyakabawat@gmail.com>
2023-03-07 12:15:16 +0530

    update

--- controller/logout/index.js
+++
@@ -1,4 +0,0 @@
-exports.logout = (req, res) => {
-    res.clearCookie('auth')
-    res.send("logout")
-}
--- index.js
+++ index.js
@@ -1,64 +1,10 @@
 const express = require('express')
-const http = require('http')
 const cors = require('cors')
 const app = express()
-const dotenv = require('dotenv').config()
-const userAuth = require('./router')
-const server = http.createServer(app)
-const socketIO = require('socket.io')
-
-const { socketModal } = require('./controller/connection')
-app.use(express.json())
-const corsOptions = {
-    origin: "http://localhost:3000",
-};
-
-app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: true }))
-
-const io = new socketIO.Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-      },
-
+app.use(cors())
+app.get("/", (req, res) => {
+    res.send("hello word")
 })
-
-app.use(userAuth)
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`click here http://localhost:${process.env.PORT}`)
 })
-
-// socket data 
-io.on('connection', socket => {
-    socket.on('join', user => {
-        socketDataHandal(socket, user)
-    })
-    socket.on('massage', data => {
-        io.to(data.receiver.chatID).emit("reciveMsg", {
-            ...data
-        })
-    })
-    socket.on('updateSocket', profile => {
-        profile && socketUpdate(socket, profile)
-    })
-
-    socket.on('refresh', data => {
-        socketUpdate(socket, { user: data })
-        socket.broadcast.emit('refreshed', data)
-    })
-})
-
-const socketDataHandal = async (socket, user) => {
-    const SocketData = new socketModal({
-        [user]: socket.id,
-        user: user
-    })
-    const responce = await SocketData.save()
-}
-
-const socketUpdate = async (socket, profile) => {
-    if (profile) {
-        await socketModal.replaceOne({ user: profile.user }, { [profile.user]: socket.id, user: profile.user })
-    }
-}
